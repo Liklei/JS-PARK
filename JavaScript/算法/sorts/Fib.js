5341a99ee0d9207826aa4b832075c9b97af90c94
lance <1208790330@qq.com>
2021-04-06 18:59:22 +0800

    add

--- JavaScript/算法/sorts/Fib.js
+++ JavaScript/算法/sorts/Fib.js
@@ -1,12 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-03-31 21:31:46
- * @LastEditTime: 2021-03-31 21:56:30
+ * @LastEditTime: 2021-03-31 22:01:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \knowTech\JS-PARK\JavaScript\算法\sorts\Fib.js
  */
-
+/**
+ * 
+ * 规则复现就需要使用递归
+ */
 function fib(n) {
     return (n === 1||n === 0) ? 1 : fib(n - 1) + fib(n - 2)
 }
