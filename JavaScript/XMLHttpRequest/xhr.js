7b34c4fe0a9c795956c88fd47698a954e7a92f9e
kailei_li <kailei_li@intsig.net>
2021-06-02 19:12:12 +0800

    add

--- JavaScript/XMLHttpRequest/xhr.js
+++ JavaScript/XMLHttpRequest/xhr.js
@@ -1,22 +1,33 @@
-/*
- * @Author: your name
- * @Date: 2021-03-04 21:38:16
- * @LastEditTime: 2021-03-04 21:47:00
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \知识点梳理\JS-PARK\JavaScript\XMLHttpRequest\xhr.js
- */
-function handler(txt){
-    console.log(txt)
-}
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("load", function (res) {
-  console.log(res);
-});
-xhr.addEventListener("progress", handler("updateProgress"));
-xhr.addEventListener("load" , handler("transferComplete"));
-xhr.addEventListener("error", handler("transferFailed"));
-xhr.addEventListener("abort", handler("transferCanceled"));
+const READT_STATE_CODE = ["请求还未初始化", "已建立服务器链接", "请求已接受", "正在处理请求", "请求已完成并且响应已准备好"]
 
-xhr.open("GET", "http://115.159.44.133:8992/api/v4/getutctime");
-xhr.send();
+
+function createAjax() {
+  return window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject()
+}
+
+function handler(txt){
+  console.log(txt)
+}
+
+var xhr = createAjax()
+
+xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+
+xhr.onreadystatechange = function(){
+  if (xhr.readyState === 2) {
+    console.log(READT_STATE_CODE[xhr.readyState])
+  } else {
+    console.dir({
+      readyState: xhr.readyState,
+      es: READT_STATE_CODE[xhr.readyState]
+    })
+  }
+}
+
+xhr.addEventListener("progress", handler("updateProgress"))
+xhr.addEventListener("load" , handler("transferComplete"))
+xhr.addEventListener("error", handler("transferFailed"))
+xhr.addEventListener("abort", handler("transferCanceled"))
+
+xhr.open("GET", "http://115.159.44.133:8992/api/v4/getutctime", true)
+xhr.send()
